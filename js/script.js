051b9c8743fe8c4f9c3446c1e03acb389be94bbf
slegka0 <44324112+slegka0@users.noreply.github.com>
2019-02-09 14:47:14 +0300

    Анимация для незаполненных полей

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 var popup = document.querySelector(".write-us");
 var close_button = popup.querySelector(".close-button");
-var write_us_form = popup.querySelector(".write_us_form");
+var write_us_form = popup.querySelector(".write-us-form");
 var link = document.querySelector(".contacts-button");
-var input_name = document.getElementById("name");
+var input_name = popup.querySelector(".write-us-name");
+var input_email = popup.querySelector(".write-us-email");
+var input_text = popup.querySelector(".text-form");
 
 link.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -11,6 +13,40 @@ link.addEventListener("click", function (evt) {
 
 close_button.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.remove("modal-show");
+  popup.classList.remove("modal-show");
+  input_name.classList.remove("modal-error");
+  input_email.classList.remove("modal-error");
+  input_text.classList.remove("modal-error");
+  popup.classList.remove("modal-error-shake");
+});
+
+write_us_form.addEventListener("submit", function (evt) {
+  input_name.classList.remove("modal-error");
+	if (!input_name.value) {
+    evt.preventDefault();
+    input_name.classList.remove("modal-error");
+    input_name.classList.add("modal-error");
+    popup.classList.add("modal-error-shake");
+  }
+});
+
+write_us_form.addEventListener("submit", function (evt) {
+  input_email.classList.remove("modal-error");
+	if (!input_email.value) {
+    evt.preventDefault();
+    input_email.classList.remove("modal-error");
+    input_email.classList.add("modal-error");
+    popup.classList.add("modal-error-shake");
+  }
+});
+
+write_us_form.addEventListener("submit", function (evt) {
+  input_text.classList.remove("modal-error");
+	if (!input_text.value) {
+    evt.preventDefault();
+    input_text.classList.remove("modal-error");
+    input_text.classList.add("modal-error");
+    popup.classList.add("modal-error-shake");
+  }
 });
 
